9a0ac4003f3384f080375081aa6647b10d9955ae
Hans Kristian Smedsrød <eplehans@gmail.com>
2021-11-01 14:09:39 +0100

    animation tweaks

--- chat/main.js
+++ chat/main.js
@@ -11,6 +11,8 @@
      );
  };
  
+
+
  
  function init() {
      /**
@@ -19,6 +21,27 @@
  
     document.body.setAttribute("data-touch", isTouchDevice());
     document.body.setAttribute("data-loaded", true);
+
+
+    function startHighLighting(item, startTime, endTime) {    
+        setInterval(function() {  
+            setInterval(function() {  
+                item.addClass('active');
+                setInterval(function() {  
+                    item.addClass('next');  
+                }, 2000);  
+            }, endTime);
+        }, startTime);
+    }
+    
+    $(".chat-list-item").each(function(index) {  
+        var item = $(this),  
+        baseTime = 2000,  
+        highlightTime = baseTime * index,  
+        unhighlightTime = baseTime * (index + 1);  
+    
+        startHighLighting(item, highlightTime, unhighlightTime);  
+    });
 
  }
  
