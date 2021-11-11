864792957b7d10dc3eb2e050ac603c2c81c72ee0
Hans Kristian Smedsrød <eplehans@gmail.com>
2021-11-11 16:30:08 +0100

    eksperimentering

--- chat/main.js
+++ chat/main.js
@@ -1,68 +1,37 @@
- 
- let isTouchDevice = function () {
-     return (
-         !!(typeof window !== 'undefined' &&
-             ('ontouchstart' in window ||
-                 (window.DocumentTouch &&
-                     typeof document !== 'undefined' &&
-                     document instanceof window.DocumentTouch))) ||
-         !!(typeof navigator !== 'undefined' &&
-             (navigator.maxTouchPoints || navigator.msMaxTouchPoints))
-     );
- };
- 
- 
- function init() {
-     /**
-     * DOM elements
-     */     
- 
-    document.body.setAttribute("data-touch", isTouchDevice());
-    document.body.setAttribute("data-loaded", true);
-
-    function scrollAnimation($el){
-        $el.scroll();
-
-        $el.animate({
-            scrollTop: 1000
-        }, 2000);
-    }
 
 
-    function startHighLighting(item, startTime, endTime) {    
+function startHighLighting(item, startTime, endTime) {    
+    setInterval(function() {  
         setInterval(function() {  
+            item.addClass('active');
+            
             setInterval(function() {  
-                item.addClass('active');
-                
-                setInterval(function() {  
-                    item.addClass('next');
-                }, 3000);  
-            }, endTime);
-        }, startTime);
-    }
-    
-    $(".animItem").each(function(index) {  
-        var item = $(this),  
-        baseTime = 2000,  
-        highlightTime = baseTime * index,  
-        unhighlightTime = baseTime * (index + 1);  
-    
-        startHighLighting(item, highlightTime, unhighlightTime);
+                item.addClass('next');
+            }, 3000);  
+        }, endTime);
+    }, startTime);
+}
 
-        if(index > 5){
-            //scrollAnimation($('.chat-list'));
-        }
+$(".animItem").each(function(index) {  
+    var item = $(this),  
+    baseTime = 2000,  
+    highlightTime = baseTime * index,  
+    unhighlightTime = baseTime * (index + 1);  
 
-    });
+    startHighLighting(item, highlightTime, unhighlightTime);
 
- }
+    if(index > 5){
+        //scrollAnimation($('.chat-list'));
+    } 
  
- /**
-  * Wait for document ready to fire dom dependent stuf
-  */
+});
+
+
+
  
- window.addEventListener('load', function () {
-     init();
- });
+ $( document ).ready(function() {
+    console.log( "ready!" );
+});
+
  
  
